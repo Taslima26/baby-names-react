603c7e6215f718f986af44c7afafc6f23078f7ba
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-09-25 13:02:11 +0100

    Updates

---
+++ precache-manifest.22ed486ddff2e2ac6498a0edb1f58702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077da901904a343275717fe4f4ec39c9",
+    "url": "/baby-names-react/index.html"
+  },
+  {
+    "revision": "ab6fb0c478c2ddeff971",
+    "url": "/baby-names-react/static/css/main.43550f59.chunk.css"
+  },
+  {
+    "revision": "fa79d9039abe74373926",
+    "url": "/baby-names-react/static/js/2.94d81c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/baby-names-react/static/js/2.94d81c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6fb0c478c2ddeff971",
+    "url": "/baby-names-react/static/js/main.17483597.chunk.js"
+  },
+  {
+    "revision": "b5d680268637e3d39921",
+    "url": "/baby-names-react/static/js/runtime-main.c2020224.js"
+  }
+]);
